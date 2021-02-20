0d56431dc55ae46a91b1ed2974f34c83bb0e5f68
nafeesanaz <naznafisa53@gmail.com>
2021-02-20 23:09:28 +0000

    main activity has now been edited to add some new recipes

--- app/src/main/java/com/example/recipes/MainActivity.java
+++ app/src/main/java/com/example/recipes/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.recipes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView myRV;
+    RecyclerViewAdapter myAdapter;
+
+    List<Recipes> recipes1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recipes1 = new ArrayList<>();
+        recipes1.add();
     }
 }
