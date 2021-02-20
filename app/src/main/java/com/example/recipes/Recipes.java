7a5f629b06b32d62c6c0286ae4f0cbe55d2649ac
nafeesanaz <naznafisa53@gmail.com>
2021-02-20 17:10:53 +0000

    variables on Recipes.java file

--- app/src/main/java/com/example/recipes/Recipes.java
+++ app/src/main/java/com/example/recipes/Recipes.java
@@ -1,4 +1,10 @@
 package com.example.recipes;
 
 public class Recipes {
+    private String RecipeName;
+    private String RecipeIngredient;
+    private String RecipeMethodTitle;
+    private String Recipe;
+    private int Thumbnail;
+
 }
