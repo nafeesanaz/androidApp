5609ab59c4172e4cbb17e1f53a18e44edf6e692d
nafeesanaz <naznafisa53@gmail.com>
2021-02-20 17:24:29 +0000

    recipes file

--- app/src/main/java/com/example/recipes/Recipes.java
+++ app/src/main/java/com/example/recipes/Recipes.java
@@ -7,4 +7,33 @@ public class Recipes {
     private String Recipe;
     private int Thumbnail;
 
+    public Recipes(String name, String recipeIngredient,String recipeMethodTitle, String recipe, int thumbnail)
+    {
+        RecipeName = name;
+        RecipeIngredient = recipeIngredient;
+        RecipeMethodTitle = recipeMethodTitle;
+        Recipe = recipe;
+        Thumbnail = thumbnail;
+
+    }
+
+    public String getRecipeName() {
+        return RecipeName;
+    }
+
+    public String getRecipeIngredient() {
+        return RecipeIngredient;
+    }
+
+    public String getRecipeMethodTitle() {
+        return RecipeMethodTitle;
+    }
+
+    public String getRecipe() {
+        return Recipe;
+    }
+
+    public int getThumbnail() {
+        return Thumbnail;
+    }
 }
